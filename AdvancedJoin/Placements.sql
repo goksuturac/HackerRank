a89228786bb91632fc2dd0bb7966753d88030b14
Göksu Turaç <turacgoksu@gmail.com>
2024-08-03 20:29:43 +0300

    problems solved

---
+++ AdvancedJoin/Placements.sql
@@ -0,0 +1,5 @@
+SELECT S.Name FROM Students S 
+JOIN Friends F ON S.ID = F.ID 
+JOIN Packages P1 ON S.ID = P1.ID
+JOIN Packages P2 ON F.Friend_ID = P2.ID
+WHERE P2.Salary > P1.Salary ORDER BY P2.Salary;
